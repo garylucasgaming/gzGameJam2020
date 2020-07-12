20fa28f91e064d0df676ec1c4b282cf70c27af50
garylucasgaming <paradoxsc2@gmail.com>
2020-07-12 06:13:48 -0400

    asd

---
+++ Gamejam2020/Assets/Scripts/CashierWinZone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CashierWinZone : MonoBehaviour
+{
+    [SerializeField] private GameManager GM;
+    private List<bool> winList; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+
+        if(player.gameObject.name == "Player")
+        {
+
+            foreach(GameObject toggle in GM._toggleList)
+            {
+                if(toggle.gameObject.transform.Find("Background").transform.Find("Checkmark").gameObject.activeSelf)
+                {
+                    winList.Add(true);
+                }
+
+            }
+
+            if(winList.Count == GM._groceryList.Count)
+            {
+                GM.WinPanel.SetActive(true);
+                Time.timeScale = 0;
+                
+            }
+
+        }
+    }
+
+
+
+}
---
+++ Gamejam2020/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private GameManager GM;
+    public float countdownNumberInSeconds = 600;
+    public float timer;
+
+    void Start()
+    {
+        timer = countdownNumberInSeconds;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            print("you lose");
+
+            
+            GM.LosePanel.SetActive(true);
+            Time.timeScale = 0 ;
+        }
+    }
+
+    
+
+
+}
